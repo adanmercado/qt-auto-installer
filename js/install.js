ca88db96565e54f836d50de28a499f73edd8c382
Adan Mercado <adanmercado.dev@gmail.com>
2023-06-12 00:20:07 -0600

    Create macos scripts

---
+++ js/install.js
@@ -0,0 +1,98 @@
+let CLICK_DELAY = 2000;
+
+function Controller() {
+    installer.installationFinished.connect(function() {
+        gui.clickButton(buttons.NextButton, CLICK_DELAY);
+    });
+
+    installer.currentPageChanged.connect(function() {
+        let widget = gui.currentPageWidget();
+        console.log("Page: " + widget.objectName + " ('" + widget.title + "')");
+    });
+}
+
+Controller.prototype.WelcomePageCallback = function() {
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.IntroductionPageCallback = function() {
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.CredentialsPageCallback = function() {
+    let page = gui.pageWidgetByObjectName('CredentialsPage');
+    page.loginWidget.EmailLineEdit.setText(QT_ACCOUNT_USERNAME);
+    page.loginWidget.PasswordLineEdit.setText(QT_ACCOUNT_PASSWORD);
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.ComponentSelectionPageCallback = function() {
+    let widget = gui.currentPageWidget();
+
+    let archiveCheckBox = gui.findChild(widget, "Archive");
+    archiveCheckBox.click();
+
+    // Deselect LTS checkbox
+    /*let ltsCheckBox = gui.findChild(widget, "Latest supported releases");
+    ltsCheckBox.click();
+    */
+    
+    let previewCheckBox = gui.findChild(widget, "Preview");
+    previewCheckBox.click();
+
+    let fetchButton = gui.findChild(widget, "FetchCategoryButton");
+    if(fetchButton)
+        fetchButton.click();
+
+    let packagesToInstall = COMPONENTS;
+    packagesToInstall.forEach(function(package) {
+        widget.selectComponent(package);
+    });
+
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.TargetDirectoryPageCallback = function() {
+    let widget = gui.currentPageWidget();
+    widget.TargetDirectoryLineEdit.setText(QT_INSTALLATION_DIR);
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.PerformInstallationPageCallback = function() {
+    //gui.clickButton(buttons.CommitButton, CLICK_DELAY);
+}
+
+Controller.prototype.LicenseAgreementPageCallback = function() {
+    gui.currentPageWidget().AcceptLicenseCheckBox.checked = true;
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.ObligationsPageCallback = function() {
+    let page = gui.pageWidgetByObjectName("ObligationsPage");
+    page.obligationsAgreement.setChecked(true);
+
+    let individualCheckbox = gui.findChild(page, "IndividualPerson")
+    if(individualCheckbox)
+        individualCheckbox.checked = true;
+
+    page.completeChanged();
+    gui.clickButton(buttons.NextButton);
+}
+
+Controller.prototype.ReadyForInstallationPageCallback = function() {
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
+
+Controller.prototype.FinishedPageCallback = function() {
+    // "Launch QtCreator"
+    gui.currentPageWidget().RunItCheckBox.checked = RUN_QT_CREATOR;
+    gui.clickButton(buttons.FinishButton, CLICK_DELAY);
+}
+
+Controller.prototype.DynamicTelemetryPluginFormCallback = function() {
+    let widget = gui.currentPageWidget();
+    let radioButtons = widget.TelemetryPluginForm.statisticGroupBox;
+    radioButtons.disableStatisticRadioButton.checked = true;
+
+    gui.clickButton(buttons.NextButton, CLICK_DELAY);
+}
---
+++ macos/install-qt.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ******************************************************************************************
+# *                    Author: Adán Mercado <adanmercado.dev@gmail.com>                    *
+# *                    Github: https://github.com/adanmercado                              *
+# ******************************************************************************************
+
+SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd -P)
+
+QT_INSTALLER_DOWNLOAD_URL="https://download.qt.io/official_releases/online_installers/qt-unified-mac-x64-online.dmg"
+QT_INSTALLER_FILENAME=qt-installer.dmg
+QT_TEMP_INSTALLATION_FOLDER_NAME=.qt-install
+
+QT_INSTALL_SCRIPT_FILENAME=install.js
+QT_INSTALL_SCRIPT_PATH=$SCRIPT_PATH/../js/$QT_INSTALL_SCRIPT_FILENAME
+
+cd ~
+if [[ -d "${QT_TEMP_INSTALLATION_FOLDER_NAME}" ]]
+then
+    rm -rf $QT_TEMP_INSTALLATION_FOLDER_NAME
+fi
+
+mkdir $QT_TEMP_INSTALLATION_FOLDER_NAME && cd $QT_TEMP_INSTALLATION_FOLDER_NAME
+
+if [[ ! -f "${QT_INSTALLER_FILENAME}" ]]
+then
+    wget $QT_INSTALLER_DOWNLOAD_URL -O $QT_INSTALLER_FILENAME
+fi
+
+hdiutil attach $QT_INSTALLER_FILENAME
+
+INSTALLER_NAME=$(ls /Volumes | grep "qt-unified-macOS")
+
+cp $QT_INSTALL_SCRIPT_PATH ./$QT_INSTALL_SCRIPT_FILENAME
+
+echo "" >> $QT_INSTALL_SCRIPT_FILENAME;
+cat $SCRIPT_PATH/../js/config.js >> $QT_INSTALL_SCRIPT_FILENAME
+echo "" >> $QT_INSTALL_SCRIPT_FILENAME;
+
+/Volumes/$INSTALLER_NAME/$INSTALLER_NAME.app/Contents/MacOS/$INSTALLER_NAME \
+    --verbose \
+    --no-default-installations \
+    --accept-messages \
+    --script "$QT_INSTALL_SCRIPT_FILENAME"
+
+hdiutil detach /Volumes/$INSTALLER_NAME
+
+cd ~
+rm -rf $QT_TEMP_INSTALLATION_FOLDER_NAME
